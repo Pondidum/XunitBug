263461b7f2ee57e65ddb73b51924e0da402dc27c
andy <andy@andydote.co.uk>
2015-06-29 22:50:54 +0100

    initial version

---
+++ XunitBug/Tests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace XunitBug
+{
+	public interface IEventStore
+	{
+		int? GetLatestSequenceFor<TKey>(TKey aggegateID);
+	}
+
+	public class TestEventStore : IEventStore
+	{
+		public int? Value { get; set; }
+
+		public int? GetLatestSequenceFor<TKey>(TKey aggegateID)
+		{
+			return Value;
+		}
+	}
+
+	public class Tests
+	{
+		[Fact]
+		public void When_faking_a_value()
+		{
+			DoTest(new TestEventStore());
+		}
+
+		public void DoTest(IEventStore store)
+		{
+			var test = store.GetLatestSequenceFor(Guid.NewGuid());
+
+			Assert.Equal(null, test);
+		}
+	}
+}
